de2a5da0881b3d7de4b1409de41c91841ea80ebb
Bogusław K. Siemiątkowski <kontakt@spizarniaregio.pl>
2025-10-13 22:40:45 +0200

    nowe metadata files

--- app/manifest.ts
+++ app/manifest.ts
@@ -1,4 +1,3 @@
-// app/manifest.ts (wersja minimalna)
 import type { MetadataRoute } from 'next';
 
 export default function manifest(): MetadataRoute.Manifest {
@@ -14,46 +13,17 @@ export default function manifest(): MetadataRoute.Manifest {
     icons: [
       {
         src: '/favicon.ico',
-        sizes: '32x32',
+        sizes: 'any',
         type: 'image/x-icon',
       },
       {
-        src: '/favicon-16x16.png',
-        sizes: '16x16',
-        type: 'image/png',
-      },
-      {
-        src: '/favicon-32x32.png',
-        sizes: '32x32',
-        type: 'image/png',
-      },
-      {
         src: '/android-chrome-192x192.png',
         sizes: '192x192',
         type: 'image/png',
-        purpose: 'any',
-      },
-      {
-        src: '/android-chrome-192x192.png',
-        sizes: '192x192',
-        type: 'image/png',
-        purpose: 'maskable',
       },
       {
         src: '/android-chrome-512x512.png',
         sizes: '512x512',
-        type: 'image/png',
-        purpose: 'any',
-      },
-      {
-        src: '/android-chrome-512x512.png',
-        sizes: '512x512',
-        type: 'image/png',
-        purpose: 'maskable',
-      },
-      {
-        src: '/apple-touch-icon.png',
-        sizes: '180x180',
         type: 'image/png',
       },
     ],
