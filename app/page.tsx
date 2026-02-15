6e5361917ad94dbdd09c00c8747b7009b7b6940f
Bogusław K. Siemiątkowski <kontakt@spizarniaregio.pl>
2026-02-15 15:57:18 +0100

    zmiana designu strony najlepsza

--- app/page.tsx
+++ app/page.tsx
@@ -62,10 +62,35 @@ export default function HomePage() {
         <p className={styles.aboutDescription}>
           Scopri insieme a noi i produttori italiani di cibo e bevande che
           vendono i loro prodotti online. Trova specialità difficili da reperire
-          nella tua zona: prodotti artigianali, regionali e di nicchia. Piazza
-          Virtuale è la tua guida dove raccogliamo in un unico posto gli
-          e-shop alimentari dei produttori di tutta Italia.
+          nella tua zona: prodotti artigianali, regionali e di nicchia.
+          <br />
+          <br /> Piazza Virtuale è la tua guida dove raccogliamo in un unico
+          posto gli e-shop alimentari dei produttori di tutta Italia.
         </p>
+        <svg
+          className={styles.aboutFlag}
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 481.1 274.45"
+          aria-hidden="true"
+        >
+          <g>
+            <g>
+              <rect x="13.5" y="1.5" width="155.9" height="261.45" fill="#006837" />
+              <path d="m158.9,274.45H0V10h158.9v264.45Zm-155.9-3h152.9V13H3v258.45Z" fill="#231f20" />
+              <path d="m170.9,264.45H12V0h158.9v264.45Zm-155.9-3h152.9V3H15v258.45Z" fill="#231f20" />
+            </g>
+            <g>
+              <rect x="168.89" y="1.5" width="155.9" height="261.45" fill="#f1f2f1" />
+              <path d="m314.29,274.45h-158.9V10h158.9v264.45Zm-155.9-3h152.9V13h-152.9v258.45Z" fill="#231f20" />
+              <path d="m326.29,264.45h-158.9V0h158.9v264.45Zm-155.9-3h152.9V3h-152.9v258.45Z" fill="#231f20" />
+            </g>
+            <g>
+              <rect x="323.71" y="1.5" width="155.9" height="261.45" fill="#cd212a" />
+              <path d="m469.11,274.45h-158.9V10h158.9v264.45Zm-155.9-3h152.9V13h-152.9v258.45Z" fill="#231f20" />
+              <path d="m481.11,264.45h-158.9V0h158.9v264.45Zm-155.9-3h152.9V3h-152.9v258.45Z" fill="#231f20" />
+            </g>
+          </g>
+        </svg>
       </section>
     </div>
   );
